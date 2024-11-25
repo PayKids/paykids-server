198e886b42c6d3cb120c7a3e1a7a066854315fca
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-11-25 19:01:23 +0900

    [CHORE] 프로젝트 초기 설정

---
+++ src/main/java/depth/main_project/PayKids_Server/PayKidsServerApplication.java
@@ -0,0 +1,13 @@
+package depth.main_project.PayKids_Server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PayKidsServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PayKidsServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/depth/main_project/PayKids_Server/PayKidsServerApplicationTests.java
@@ -0,0 +1,13 @@
+package depth.main_project.PayKids_Server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PayKidsServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
