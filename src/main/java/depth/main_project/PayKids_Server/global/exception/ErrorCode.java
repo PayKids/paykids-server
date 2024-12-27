44c7006728d4fef0d2935673c13d9a46ee604527
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-12-27 16:45:43 +0900

    [FEAT] 서버 accessToken 발급 및 해석 로직 추가

--- src/main/java/depth/main_project/PayKids_Server/global/exception/ErrorCode.java
+++ src/main/java/depth/main_project/PayKids_Server/global/exception/ErrorCode.java
@@ -9,7 +9,9 @@ import org.springframework.http.HttpStatus;
 public enum ErrorCode {
     //Common
     INTERNAL_SERVER_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "서버에 오류가 발생하였습니다."),
-    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Request Body를 통해 전달된 값이 유효하지 않습니다.");
+    INVALID_INPUT_VALUE(HttpStatus.BAD_REQUEST, "Request Body를 통해 전달된 값이 유효하지 않습니다."),
+    USER_NOT_FOUND(HttpStatus.NOT_FOUND, "User not found"),
+    TOKEN_ERROR(HttpStatus.INTERNAL_SERVER_ERROR, "토큰 해석 문제가 발생했습니다");
 
     private final HttpStatus httpStatus;
     private final String message;
