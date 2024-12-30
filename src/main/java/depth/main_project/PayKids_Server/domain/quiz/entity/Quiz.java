c2eed9ca2806160e00e90a4cfe7d5511e1070cb6
Ju Do Hyun <wnehgus88@mju.ac.kr>
2024-12-30 20:02:53 +0900

    [FEAT] 이미지 저장 타입 변경

--- src/main/java/depth/main_project/PayKids_Server/domain/quiz/entity/Quiz.java
+++ src/main/java/depth/main_project/PayKids_Server/domain/quiz/entity/Quiz.java
@@ -4,10 +4,12 @@ import jakarta.persistence.*;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 @NoArgsConstructor
 @Entity
 @Getter
+@Setter
 @Table(name = "Quiz")
 public class Quiz {
     @Id
@@ -22,7 +24,7 @@ public class Quiz {
     private String choices;
     private String answer;
 
-    @Column(nullable = true)
+    @Column(columnDefinition = "JSON", nullable = true)
     private String imageURL;
 
     @Builder
