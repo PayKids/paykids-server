19f2bb3fc34811aad8e47d2792f973f9ca1b20ea
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-01-16 22:04:17 +0900

    [FEAT] 유저 삭제 기능 추가

--- src/main/java/depth/main_project/PayKids_Server/domain/user/UserController.java
+++ src/main/java/depth/main_project/PayKids_Server/domain/user/UserController.java
@@ -75,4 +75,18 @@ public class UserController {
 
         return ApiResult.ok(result);
     }
+
+    @Operation(summary = "프로필 사진 변경", description = "프로필 사진 변경")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "성공적으로 확인됨"),
+            @ApiResponse(responseCode = "400", description = "잘못된 입력 값"),
+            @ApiResponse(responseCode = "500", description = "서버 오류 발생")
+    })
+    @PostMapping("/delete")
+    public ApiResult<String> changeProfileImage(@RequestHeader("Authorization") String accessToken) {
+        String userUUID = tokenService.getUserUuidFromToken(accessToken);
+        String result = userService.deleteUser(userUUID);
+
+        return ApiResult.ok(result);
+    }
 }
