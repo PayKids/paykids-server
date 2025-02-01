5692644c37cd1473e171259a0019f8f3200ca80d
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-02-02 01:46:07 +0900

    [FEAT] 업적, 퀘스트 확인 기능 구현

--- src/main/java/depth/main_project/PayKids_Server/domain/user/repository/UserRepository.java
+++ src/main/java/depth/main_project/PayKids_Server/domain/user/repository/UserRepository.java
@@ -10,5 +10,6 @@ import java.util.Optional;
 public interface UserRepository extends JpaRepository<User, Long> {
     Optional<User> findByKakaoId(String kakaoId);
     Optional<User> findByUuid(String uuid);
+    Optional<User> findByNickname(String nickname);
     boolean existsByNickname(String nickname);
 }
