4a37157971582565a13eb26dfa3398a32d17968f
Ju Do Hyun <wnehgus88@mju.ac.kr>
2025-02-07 14:10:07 +0900

    [FIX] 회원 탈퇴 수정

--- src/main/java/depth/main_project/PayKids_Server/domain/quiz/repository/UserStageRepository.java
+++ src/main/java/depth/main_project/PayKids_Server/domain/quiz/repository/UserStageRepository.java
@@ -5,8 +5,10 @@ import depth.main_project.PayKids_Server.domain.quiz.entity.UserStage;
 import depth.main_project.PayKids_Server.domain.user.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface UserStageRepository extends JpaRepository<UserStage, Long> {
     Optional<UserStage> findUserStageByUserAndStageName(User user, StageName stageName);
+    List<UserStage> findAllByUser(User user);
 }
